c0b2f9447247dfecc47a0f1c8ffb1fdce6941c44
Marwan-Essam55 <marawan.elbob@gmai.com>
2026-01-31 15:49:59 +0200

    Update analytics import

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from '@vercel/analytics/next';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
