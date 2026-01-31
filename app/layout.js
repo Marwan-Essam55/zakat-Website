feaab165d13e64813a8576dd3f13e74180faee8c
Marwan-Essam55 <marawan.elbob@gmai.com>
2026-01-31 16:04:45 +0200

    Add AdSense script

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script'; // الخطوة 1: استيراد المكتبة
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
@@ -20,6 +22,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl" className="light" style={{ colorScheme: 'light' }}>
+      <head>
+        {/* الخطوة 2: كود جوجل أدسنس الخاص بك */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6776396365196393"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${inter.className} bg-slate-50 text-slate-900 antialiased`}>
         {children}
         <Analytics />
